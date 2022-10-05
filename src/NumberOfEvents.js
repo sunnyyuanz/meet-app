b5159fe1ca6209ad3b17d8ea20941cdd26e20911
sunnyyuanz <84478918+sunnyyuanz@users.noreply.github.com>
2022-10-04 23:39:16 -0400

    OOP

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
@@ -12,11 +13,15 @@ class NumberOfEvents extends Component {
       this.props.updateEvents(undefined, userInputNum);
       console.log(userInputNum);
     }
+    if (input.target.value > 32 || input.target.value < 1) {
+      this.setState({ infoText: 'Select number from 1 to 32' });
+    }
   };
 
   render() {
     return (
       <div className="numberOfEvents">
+        <ErrorAlert text={this.state.infoText} />
         <input
           type="number"
           placeholder="Specify Number of Events"
